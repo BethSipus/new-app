760b190655e29872d730196a8033a157ffb4f2ce
BethSipus <99303041+BethSipus@users.noreply.github.com>
2022-03-07 20:03:41 -0800

    Update scripts.js
    erased contents of scripts.js so begin with new file. Created new variable pokemonList. Added pokemon data to array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-alert('Hello world');
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let pokemonRepository = (function) {
+  let pokemon List = [{
+    name: 'Squirtel',
+    height: 0.5,
+    type: ['water']
+  },
+  {
+    name: 'Jigglypuff',
+    height: 0.5,
+    type: ['fairy', 'normal']
+  },
+  {
+    name: 'Snorlax',
+    height: 2.1,
+    type: ['normal']
+  },
+  }]
+}
