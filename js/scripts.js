b292073ddb1f6967dcdcea1663214a1082d2cc89
BethSipus <99303041+BethSipus@users.noreply.github.com>
2022-03-21 14:01:42 -0700

    Update scripts.js
    *added for loop over each item in pokemonList.
    *added special highlight for above a certain height

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,16 @@
     height: 2.1,
     type: ['normal']
   },
-  ]
+];
+
+pokemonList.forEach(function(pokemon){
+document.write(pokemon.name + " with type: (" + pokemon.type + ") " + "and" + " (height :" + pokemon.height + "),   ")
+});
+
+/*for (let i=0; i < pokemonList.length; i++){
+  if(pokemonList[i].height > 1){
+    document.write(pokemonList[i].name + "( height : " + pokemonList[i].height + " )" + `Wow it's a big pokemon!!!`)
+  }else {
+  document.write(pokemonList[i].name + "( height : " + pokemonList[i].height + " )")
+}
+}*/
